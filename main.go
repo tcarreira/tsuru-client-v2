3fe2b0246796b94b1a9e2e1704d65acf9df60333
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-04-06 01:20:30 -0300

    cobra-cli init

---
+++ main.go
@@ -0,0 +1,12 @@
+/*
+Copyright © 2023 tsuru-client authors
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
+*/
+package main
+
+import "github.com/tsuru/tsuru-client/cmd"
+
+func main() {
+	cmd.Execute()
+}
