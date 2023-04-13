acf3ad69f0c7112b16a18a650841f70cc89186ca
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-04-13 14:45:27 -0300

    test TestFormatAs

---
+++ internal/printer/info.go
@@ -0,0 +1,18 @@
+package printer
+
+import (
+	"io"
+)
+
+func PrintInfo(out io.Writer, format OutputType, data any) (err error) {
+	switch format {
+	case JSON:
+		return PrintJSON(out, data)
+	case YAML:
+		return PrintYAML(out, data)
+	case Table:
+		return PrintTable(out, data)
+	default:
+		return PrintTable(out, data)
+	}
+}
---
+++ internal/printer/printer.go
@@ -0,0 +1,69 @@
+package printer
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"gopkg.in/yaml.v3"
+)
+
+type OutputType string
+
+const (
+	JSON  OutputType = "json"
+	YAML  OutputType = "yaml"
+	Table OutputType = "table"
+)
+
+func FormatAs(s string) OutputType {
+	switch s {
+	case "json":
+		return JSON
+	case "yaml":
+		return YAML
+	case "table":
+		return Table
+	default:
+		return Table
+	}
+}
+
+func PrintJSON(out io.Writer, data any) error {
+	dataByte, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(out, string(dataByte))
+	return nil
+}
+
+func PrintYAML(out io.Writer, data any) error {
+	dataByte, err := yaml.Marshal(data)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(out, string(dataByte))
+	return nil
+}
+
+func PrintTable(out io.Writer, data any) (err error) {
+	if data == nil {
+		return nil
+	}
+
+	switch tData := data.(type) {
+	case []byte:
+		out.Write(tData)
+	case string:
+		fmt.Fprintln(out, tData)
+	case io.Reader:
+		_, err = io.Copy(out, tData)
+	case map[string]any:
+		for k, v := range tData {
+			fmt.Fprintf(out, "%s: %v", k, v)
+		}
+	}
+
+	return err
+}
