e4adeee2f52f49a243013f8baf48fee903620786
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-05-04 11:21:14 -0300

    add TestAppInfoManyProcesses

--- pkg/cmd/app/app_test.go
+++ pkg/cmd/app/app_test.go
@@ -465,3 +465,82 @@ Units: 3
 	assert.NoError(t, err)
 	assert.Equal(t, expected, stdout.String())
 }
+
+func TestAppInfoManyProcesses(t *testing.T) {
+	var stdout bytes.Buffer
+	result := `{
+  "name": "app1",
+  "teamowner": "myteam",
+  "cname": [
+    ""
+  ],
+  "ip": "myapp.tsuru.io",
+  "platform": "php",
+  "repository": "git@git.com:php.git",
+  "state": "dead",
+  "units": [
+    {
+      "Ip": "10.10.10.10",
+      "ID": "app1/0",
+      "Status": "started",
+      "ProcessName": "web"
+    },
+    {
+      "Ip": "9.9.9.9",
+      "ID": "app1/1",
+      "Status": "started",
+      "ProcessName": "worker"
+    },
+    {
+      "Ip": "",
+      "ID": "app1/2",
+      "Status": "pending",
+      "ProcessName": "worker"
+    }
+  ],
+  "teams": [
+    "tsuruteam",
+    "crane"
+  ],
+  "owner": "myapp_owner",
+  "deploys": 7,
+  "router": "planb"
+}`
+	expected := `Application: app1
+Platform: php
+Router: planb
+Teams: myteam (owner), tsuruteam, crane
+External Addresses: myapp.tsuru.io
+Created by: myapp_owner
+Deploys: 7
+Pool:
+Quota: 0/0 units
+
+Units [process web]: 1
++--------+---------+-------------+------+
+| Name   | Status  | Host        | Port |
++--------+---------+-------------+------+
+| app1/0 | started | 10.10.10.10 |      |
++--------+---------+-------------+------+
+
+Units [process worker]: 2
++--------+---------+---------+------+
+| Name   | Status  | Host    | Port |
++--------+---------+---------+------+
+| app1/2 | pending |         |      |
+| app1/1 | started | 9.9.9.9 |      |
++--------+---------+---------+------+
+
+`
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, result)
+	}))
+	apiClient := api.APIClientWithConfig(&tsuru.Configuration{BasePath: mockServer.URL, HTTPClient: mockServer.Client()})
+
+	appInfoCmd := newAppInfoCmd()
+	appInfoCmd.Flags().Parse([]string{"--app", "app1"})
+	err := printAppInfo(appInfoCmd, []string{}, apiClient, &stdout)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, stdout.String())
+}
