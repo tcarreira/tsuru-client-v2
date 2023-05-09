db806ba96a75b9b8d649186593770a148fd812c5
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-05-09 14:29:08 -0300

    add TestV1AppCreateTeamOwner

--- pkg/cmd/app/create_test.go
+++ pkg/cmd/app/create_test.go
@@ -40,10 +40,8 @@ func TestV1AppCreate(t *testing.T) {
 	appCreateCmd := newAppCreateCmd()
 	var stdout bytes.Buffer
 	err := appCreateRun(appCreateCmd, []string{"ble", "django"}, apiClient, &stdout)
-
 	assert.NoError(t, err)
 	assert.Equal(t, fmt.Sprintf(expectedFmt, "ble"), stdout.String())
-
 }
 
 func TestV1AppCreateEmptyPlatform(t *testing.T) {
@@ -69,8 +67,34 @@ func TestV1AppCreateEmptyPlatform(t *testing.T) {
 	appCreateCmd := newAppCreateCmd()
 	var stdout bytes.Buffer
 	err := appCreateRun(appCreateCmd, []string{"ble"}, apiClient, &stdout)
-
 	assert.NoError(t, err)
 	assert.Equal(t, fmt.Sprintf(expectedFmt, "ble"), stdout.String())
+}
 
+func TestV1AppCreateTeamOwner(t *testing.T) {
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, "application/x-www-form-urlencoded", r.Header.Get("Content-Type"))
+
+		assert.True(t, strings.HasSuffix(r.URL.Path, "/apps"))
+		assert.Equal(t, "ble", r.FormValue("name"))
+		assert.Equal(t, "django", r.FormValue("platform"))
+		assert.Equal(t, "myteam", r.FormValue("teamOwner"))
+		assert.Equal(t, "", r.FormValue("plan"))
+		assert.Equal(t, "", r.FormValue("pool"))
+		assert.Equal(t, "", r.FormValue("description"))
+		assert.Equal(t, "", r.FormValue("router"))
+		r.ParseForm()
+		assert.Nil(t, r.Form["tag"])
+
+		fmt.Fprintln(w, `{"status":"success", "repository_url":"git@tsuru.plataformas.glb.com:ble.git"}`)
+	}))
+	apiClient := api.APIClientWithConfig(&tsuru.Configuration{BasePath: mockServer.URL, HTTPClient: mockServer.Client()})
+
+	appCreateCmd := newAppCreateCmd()
+	appCreateCmd.LocalFlags().Parse([]string{"-t", "myteam"})
+	var stdout bytes.Buffer
+	err := appCreateRun(appCreateCmd, []string{"ble", "django"}, apiClient, &stdout)
+	assert.NoError(t, err)
+	assert.Equal(t, fmt.Sprintf(expectedFmt, "ble"), stdout.String())
 }
