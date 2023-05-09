7d8b86e27efd88c33b963d2879258bff38f0959c
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-05-09 14:27:15 -0300

    add TestV1AppCreateEmptyPlatform

--- pkg/cmd/app/create_test.go
+++ pkg/cmd/app/create_test.go
@@ -45,3 +45,32 @@ func TestV1AppCreate(t *testing.T) {
 	assert.Equal(t, fmt.Sprintf(expectedFmt, "ble"), stdout.String())
 
 }
+
+func TestV1AppCreateEmptyPlatform(t *testing.T) {
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, "application/x-www-form-urlencoded", r.Header.Get("Content-Type"))
+
+		assert.True(t, strings.HasSuffix(r.URL.Path, "/apps"))
+		assert.Equal(t, "ble", r.FormValue("name"))
+		assert.Equal(t, "", r.FormValue("platform"))
+		assert.Equal(t, "", r.FormValue("teamOwner"))
+		assert.Equal(t, "", r.FormValue("plan"))
+		assert.Equal(t, "", r.FormValue("pool"))
+		assert.Equal(t, "", r.FormValue("description"))
+		assert.Equal(t, "", r.FormValue("router"))
+		r.ParseForm()
+		assert.Nil(t, r.Form["tag"])
+
+		fmt.Fprintln(w, `{"status":"success", "repository_url":"git@tsuru.plataformas.glb.com:ble.git"}`)
+	}))
+	apiClient := api.APIClientWithConfig(&tsuru.Configuration{BasePath: mockServer.URL, HTTPClient: mockServer.Client()})
+
+	appCreateCmd := newAppCreateCmd()
+	var stdout bytes.Buffer
+	err := appCreateRun(appCreateCmd, []string{"ble"}, apiClient, &stdout)
+
+	assert.NoError(t, err)
+	assert.Equal(t, fmt.Sprintf(expectedFmt, "ble"), stdout.String())
+
+}
