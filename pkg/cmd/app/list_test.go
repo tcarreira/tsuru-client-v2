9bde630484bc8dbe165adb7a7028adea60d5040c
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-05-10 17:49:08 -0300

    add TestV1AppList

--- pkg/cmd/app/list_test.go
+++ pkg/cmd/app/list_test.go
@@ -1,10 +1,37 @@
 package app
 
 import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/tsuru/go-tsuruclient/pkg/tsuru"
+	"github.com/tsuru/tsuru-client/internal/api"
 )
+
+func TestV1AppList(t *testing.T) {
+	var stdout bytes.Buffer
+	result := `[{"ip":"10.10.10.10","name":"app1","units":[{"ID":"app1/0","Status":"started"}]}]`
+	expected := `+-------------+-----------+-------------+
+| Application | Units     | Address     |
++-------------+-----------+-------------+
+| app1        | 1 started | 10.10.10.10 |
++-------------+-----------+-------------+
+`
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, result)
+	}))
+	apiClient := api.APIClientWithConfig(&tsuru.Configuration{BasePath: mockServer.URL, HTTPClient: mockServer.Client()})
+
+	appListCmd := newAppListCmd()
+	err := appListCmdRun(appListCmd, []string{}, apiClient, &stdout)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, stdout.String())
+}
 
 func TestAppListIsRegistered(t *testing.T) {
 	appCmd := NewAppCmd()
