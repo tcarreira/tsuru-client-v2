dd776fad7bac55b0819d3e753da4d4671b50ee10
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-05-10 17:48:59 -0300

    implement printAppList()

--- pkg/cmd/app/list.go
+++ pkg/cmd/app/list.go
@@ -1,13 +1,17 @@
 package app
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"sort"
+	"strings"
 
 	"github.com/antihax/optional"
 	"github.com/spf13/cobra"
 	"github.com/tsuru/go-tsuruclient/pkg/tsuru"
+	"github.com/tsuru/tablecli"
 	"github.com/tsuru/tsuru-client/internal/api"
 	"github.com/tsuru/tsuru-client/internal/printer"
 )
@@ -79,5 +83,106 @@ func printAppList(out io.Writer, format printer.OutputType, simplified bool, app
 	if format == printer.JSON {
 		return printer.PrintPrettyJSON(out, apps)
 	}
+
+	table := tablecli.NewTable()
+	if simplified {
+		for _, app := range apps {
+			fmt.Fprintln(out, app.Name)
+		}
+		return nil
+	}
+
+	table.Headers = tablecli.Row([]string{"Application", "Units", "Address"})
+	for _, app := range apps {
+		var summary string
+		if app.Error == "" {
+			unitsStatus := make(map[string]int)
+			for _, unit := range app.Units {
+				if unit.Id != "" {
+					if unit.Ready != nil && *unit.Ready {
+						unitsStatus["ready"]++
+					} else {
+						unitsStatus[unit.Status]++
+					}
+				}
+			}
+			statusText := make([]string, len(unitsStatus))
+			i := 0
+			us := newUnitSorter(unitsStatus)
+			sort.Sort(us)
+			for _, status := range us.Statuses {
+				statusText[i] = fmt.Sprintf("%d %s", unitsStatus[status], status)
+				i++
+			}
+			summary = strings.Join(statusText, "\n")
+		} else {
+			summary = "error fetching units"
+			// if client.Verbosity > 0 {
+			// 	summary += fmt.Sprintf(": %s", app.Error)
+			// }
+		}
+		addrs := strings.Replace(appAddr(app), ", ", "\n", -1)
+		table.AddRow(tablecli.Row([]string{app.Name, summary, addrs}))
+	}
+	table.LineSeparator = true
+	table.Sort()
+	out.Write(table.Bytes())
 	return nil
+}
+
+type unitSorter struct {
+	Statuses []string
+	Counts   []int
+}
+
+func (u *unitSorter) Len() int {
+	return len(u.Statuses)
+}
+
+func (u *unitSorter) Swap(i, j int) {
+	u.Statuses[i], u.Statuses[j] = u.Statuses[j], u.Statuses[i]
+	u.Counts[i], u.Counts[j] = u.Counts[j], u.Counts[i]
+}
+
+func (u *unitSorter) Less(i, j int) bool {
+	if u.Counts[i] > u.Counts[j] {
+		return true
+	}
+	if u.Counts[i] == u.Counts[j] {
+		return u.Statuses[i] < u.Statuses[j]
+	}
+	return false
+}
+
+func newUnitSorter(m map[string]int) *unitSorter {
+	us := &unitSorter{
+		Statuses: make([]string, 0, len(m)),
+		Counts:   make([]int, 0, len(m)),
+	}
+	for k, v := range m {
+		us.Statuses = append(us.Statuses, k)
+		us.Counts = append(us.Counts, v)
+	}
+	return us
+}
+
+func appAddr(a tsuru.MiniApp) string {
+	var allAddrs []string
+	for _, cname := range a.Cname {
+		if cname != "" {
+			allAddrs = append(allAddrs, cname+" (cname)")
+		}
+	}
+	if len(a.Routers) == 0 {
+		if a.Ip != "" {
+			allAddrs = append(allAddrs, a.Ip)
+		}
+	} else {
+		for _, r := range a.Routers {
+			if r.Address != "" {
+				allAddrs = append(allAddrs, r.Address)
+			}
+		}
+	}
+	return strings.Join(allAddrs, ", ")
 }
