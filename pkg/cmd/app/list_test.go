dd206acc76293da2d9f2c6acae9fec03a70e0842
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-05-10 17:51:39 -0300

    add TestV1AppListUnitIsntAvailable

--- pkg/cmd/app/list_test.go
+++ pkg/cmd/app/list_test.go
@@ -54,7 +54,27 @@ func TestV1AppListDisplayAppsInAlphabeticalOrder(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, expected, stdout.String())
+}
 
+func TestV1AppListUnitIsntAvailable(t *testing.T) {
+	var stdout bytes.Buffer
+	result := `[{"ip":"10.10.10.10","name":"app1","units":[{"ID":"app1/0","Status":"pending"}]}]`
+	expected := `+-------------+-----------+-------------+
+| Application | Units     | Address     |
++-------------+-----------+-------------+
+| app1        | 1 pending | 10.10.10.10 |
++-------------+-----------+-------------+
+`
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, result)
+	}))
+	apiClient := api.APIClientWithConfig(&tsuru.Configuration{BasePath: mockServer.URL, HTTPClient: mockServer.Client()})
+
+	appListCmd := newAppListCmd()
+	err := appListCmdRun(appListCmd, []string{}, apiClient, &stdout)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, stdout.String())
 }
 
 func TestAppListIsRegistered(t *testing.T) {
