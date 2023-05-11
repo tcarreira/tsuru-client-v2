86577f4a3541cc5b9e92b27dc9692ad38eb22863
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-05-11 11:30:57 -0300

    add TestV1AppListCName

--- pkg/cmd/app/list_test.go
+++ pkg/cmd/app/list_test.go
@@ -157,6 +157,28 @@ func TestV1AppListUnitWithoutID(t *testing.T) {
 	assert.Equal(t, expected, stdout.String())
 }
 
+func TestAppListCName(t *testing.T) {
+	var stdout bytes.Buffer
+	result := `[{"ip":"10.10.10.10","cname":["app1.tsuru.io"],"name":"app1","units":[{"ID":"app1/0","Status":"started"}]}]`
+	expected := `+-------------+-----------+-----------------------+
+| Application | Units     | Address               |
++-------------+-----------+-----------------------+
+| app1        | 1 started | app1.tsuru.io (cname) |
+|             |           | 10.10.10.10           |
++-------------+-----------+-----------------------+
+`
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, result)
+	}))
+	apiClient := api.APIClientWithConfig(&tsuru.Configuration{BasePath: mockServer.URL, HTTPClient: mockServer.Client()}, nil)
+
+	appListCmd := newAppListCmd()
+	err := appListCmdRun(appListCmd, []string{}, apiClient, &stdout)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, stdout.String())
+}
+
 func TestAppListIsRegistered(t *testing.T) {
 	appCmd := NewAppCmd()
 	assert.NotNil(t, appCmd)
