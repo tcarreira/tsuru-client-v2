78faa8df5fcfb9274907da63c927e7f814504a4a
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-05-22 15:42:38 -0300

    implement appLogCmdRun

--- pkg/cmd/app/log.go
+++ pkg/cmd/app/log.go
@@ -5,11 +5,19 @@
 package app
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/tsuru/tsuru-client/internal/api"
+	"github.com/tsuru/tsuru-client/internal/printer"
 )
 
 func newAppLogCmd() *cobra.Command {
@@ -55,5 +63,107 @@ $ tsuru app log -l 50 -f myapp`,
 }
 
 func appLogCmdRun(cmd *cobra.Command, args []string, apiClient *api.APIClient, out io.Writer) error {
+	appName, unitID, err := appNameAndUnitIDFromArgsOrFlags(cmd, args)
+	if err != nil {
+		return err
+	}
+	cmd.SilenceUsage = true
+
+	request, err := apiClient.NewRequest("GET", "/apps/"+appName+"/log", nil)
+	if err != nil {
+		return err
+	}
+	qs := make(url.Values)
+	qs.Set("lines", cmd.Flag("lines").Value.String())
+	if unitID != "" {
+		qs.Set("unit", unitID)
+	}
+	if cmd.Flag("source").Value.String() != "" {
+		qs.Set("source", cmd.Flag("source").Value.String())
+	}
+	if isFollow, _ := cmd.Flags().GetBool("follow"); isFollow {
+		qs.Set("follow", "1")
+	}
+	request.URL.RawQuery = qs.Encode()
+	httpResponse, err := apiClient.RawHTTPClient.Do(request)
+	if err != nil {
+		return err
+	}
+	defer httpResponse.Body.Close()
+
+	if httpResponse.StatusCode == http.StatusNoContent {
+		return nil
+	}
+
+	formatter := logFormatter{
+		noDate:   func() bool { v, _ := cmd.Flags().GetBool("no-date"); return v }(),
+		noSource: func() bool { v, _ := cmd.Flags().GetBool("no-source"); return v }(),
+	}
+	dec := json.NewDecoder(httpResponse.Body)
+	for {
+		err = formatter.Format(out, dec)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Fprintf(out, "Error: %v", err)
+			}
+			break
+		}
+	}
 	return nil
+}
+
+type logFormatter struct {
+	noDate   bool
+	noSource bool
+}
+type log struct {
+	Date    time.Time
+	Message string
+	Source  string
+	Unit    string
+}
+
+func (f logFormatter) Format(out io.Writer, dec *json.Decoder) error {
+	var logs []log
+	err := dec.Decode(&logs)
+	if err != nil {
+		if err == io.EOF {
+			return err
+		}
+		buffered := dec.Buffered()
+		bufferedData, _ := io.ReadAll(buffered)
+		return fmt.Errorf("unable to parse json: %v: %q", err, string(bufferedData))
+	}
+	colorify := printer.Colorify{
+		DisableColors: viper.IsSet("disable-colors"),
+	}
+	for _, l := range logs {
+		prefix := f.prefix(l)
+
+		if prefix == "" {
+			fmt.Fprintf(out, "%s\n", l.Message)
+		} else {
+			fmt.Fprintf(out, "%s %s\n", colorify.Colorfy(prefix, "blue", "", ""), l.Message)
+		}
+	}
+	return nil
+}
+
+func (f logFormatter) prefix(l log) string {
+	parts := make([]string, 0, 2)
+	if !f.noDate {
+		parts = append(parts, l.Date.In(time.Local).Format("2006-01-02 15:04:05 -0700"))
+	}
+	if !f.noSource {
+		if l.Unit != "" {
+			parts = append(parts, fmt.Sprintf("[%s][%s]", l.Source, l.Unit))
+		} else {
+			parts = append(parts, fmt.Sprintf("[%s]", l.Source))
+		}
+	}
+	prefix := strings.Join(parts, " ")
+	if prefix != "" {
+		prefix = prefix + ":"
+	}
+	return prefix
 }
