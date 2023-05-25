5795c404a44c684a3ae50197a9adccbf44cd3aa3
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-05-25 10:07:16 -0300

    add TestNativeLoginWithoutEmailFromArg

--- pkg/cmd/auth/native_test.go
+++ pkg/cmd/auth/native_test.go
@@ -20,6 +20,28 @@ import (
 func TestNativeLogin(t *testing.T) {
 	viper.Set("token", "") // concurrent with TestLoginCmdRunErr
 	result := `{"token": "sometoken", "is_admin": true}`
+	expected := "Email: Password: \nSuccessfully logged in!\n"
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		assert.True(t, strings.HasSuffix(r.URL.Path, "/users/foo@foo.com/tokens"))
+		assert.Equal(t, "application/x-www-form-urlencoded", r.Header.Get("Content-Type"))
+		assert.Equal(t, "chico", r.FormValue("password"))
+		fmt.Fprint(w, result)
+	}))
+
+	tsuruCtx := tsuructx.TsuruContextWithConfig(&tsuru.Configuration{BasePath: mockServer.URL, HTTPClient: mockServer.Client()}, nil)
+	tsuruCtx.AuthScheme = "native"
+	tsuruCtx.Stdin = &tsuructx.FakeStdin{Reader: strings.NewReader("foo@foo.com\nchico\n")}
+
+	cmd := NewLoginCmd()
+	err := loginCmdRun(cmd, []string{}, tsuruCtx)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, tsuruCtx.Stdout.(*strings.Builder).String())
+}
+
+func TestNativeLoginWithoutEmailFromArg(t *testing.T) {
+	viper.Set("token", "") // concurrent with TestLoginCmdRunErr
+	result := `{"token": "sometoken", "is_admin": true}`
 	expected := "Password: \nSuccessfully logged in!\n"
 	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, "POST", r.Method)
