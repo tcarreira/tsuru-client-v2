878c2d3f5054f515c0e0e86c2406548b594776a6
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-06-01 08:48:09 -0300

    app deploy wip (not working)

--- pkg/cmd/app/deploy.go
+++ pkg/cmd/app/deploy.go
@@ -5,6 +5,11 @@
 package app
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
 	"github.com/spf13/cobra"
 	"github.com/tsuru/tsuru-client/internal/tsuructx"
 )
@@ -50,9 +55,71 @@ To deploy using container file ("docker build" mode):
 	appDeployCmd.Flags().StringP("message", "m", "", "A message describing this deploy")
 	appDeployCmd.Flags().BoolP("files-only", "f", false, "Enables single file deployment into the root of the app's tree")
 	appDeployCmd.Flags().String("dockerfile", "", "Container file")
+	appDeployCmd.Flags().Bool("new-version", false, "Creates a new version for the current deployment while preserving existing versions")
+	appDeployCmd.Flags().Bool("override-old-versions", false, "Force replace all deployed versions by this new deploy")
 	return appDeployCmd
 }
 
 func appDeployCmdRun(cmd *cobra.Command, args []string, tsuruCtx *tsuructx.TsuruContext) error {
+	appName := cmd.Flag("app").Value.String()
+	if appName == "" && len(args) > 0 {
+		appName = args[0]
+		args = args[1:]
+	}
+
+	if appName == "" {
+		return fmt.Errorf("no app was provided. Please provide an app name")
+	}
+
+	if cmd.Flag("image").Value.String() == "" && cmd.Flag("dockerfile").Value.String() == "" && len(args) == 0 {
+		return fmt.Errorf("you should provide at least one file, Docker image name or Dockerfile to deploy")
+	}
+
+	if cmd.Flag("image").Value.String() != "" && len(args) > 0 {
+		return fmt.Errorf("you can't deploy files and docker image at the same time")
+	}
+
+	if cmd.Flag("image").Value.String() != "" && cmd.Flag("dockerfile").Value.String() != "" {
+		return fmt.Errorf("you can't deploy container image and container file at same time")
+	}
+
+	cmd.SilenceUsage = true
+
+	values := url.Values{}
+	values.Set("origin", "app-deploy")
+	if cmd.Flag("image").Value.String() != "" {
+		values.Set("origin", "image")
+	}
+	if msg := cmd.Flag("message").Value.String(); msg != "" {
+		values.Set("message", msg)
+	}
+	if newV := cmd.Flag("new-version").Value.String(); newV == "true" {
+		values.Set("new-version", "true")
+	}
+	if overrideV := cmd.Flag("override-old-versions").Value.String(); overrideV == "true" {
+		values.Set("override-versions", "true")
+	}
+
+	requestReader, _ := io.Pipe()
+	request, err := tsuruCtx.NewRequest("POST", "/apps/"+appName+"/deploy", requestReader)
+	if err != nil {
+		return err
+	}
+	httpResponse, err := tsuruCtx.RawHTTPClient().Do(request)
+	if err != nil {
+		return err
+	}
+	defer httpResponse.Body.Close()
+	if httpResponse.StatusCode == http.StatusNotFound {
+		return fmt.Errorf("app %q not found", appName)
+	}
+
+	// ----
+	debugWriter := io.Discard
+	debug := tsuruCtx.Verbosity > 0 // e.g. --verbosity 2
+	if debug {
+		debugWriter = tsuruCtx.Stderr
+	}
+	_ = debugWriter
 	return nil
 }
