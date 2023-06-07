df76848e998a77def4c6bf2841c5adfe226d7478
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-06-07 16:36:36 -0300

    fixing goreleaser

--- scripts/generate_metadata.sh
+++ scripts/generate_metadata.sh
@@ -11,7 +11,7 @@ fail=0
 [ -z "${META_PROJECT_NAME:-}" ] && echo "No META_PROJECT_NAME env" && _=$(( fail+=1 ))
 [ -z "${META_VERSION:-}" ] && echo "No META_VERSION env" && _=$(( fail+=1 ))
 [ -z "${META_TAG:-}" ] && echo "No META_TAG env" && _=$(( fail+=1 ))
-[ -z "${META_PREVIOUS_TAG:-}" ] && echo "No META_PREVIOUS_TAG env" && _=$(( fail+=1 ))
+# [ -z "${META_PREVIOUS_TAG:-}" ] && echo "No META_PREVIOUS_TAG env" && _=$(( fail+=1 ))
 [ -z "${META_COMMIT:-}" ] && echo "No META_COMMIT env" && _=$(( fail+=1 ))
 [ -z "${META_DATE:-}" ] && echo "No META_DATE env" && _=$(( fail+=1 ))
 [ "$fail" -gt 0 ] && exit 1
